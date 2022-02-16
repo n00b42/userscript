c4c9030901cd92a4db369f87a420f71df44a1c46
n00b42 <github.com@mail.msdigital.de>
2022-02-16 16:56:45 +0100

    Create 9gag-video-filter.user.js

---
+++ scripts/9gag-video-filter.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        9gag video filter
+// @version     0.0.1
+// @namespace   de.n00b42
+// @description 9gag videos filter
+//
+// @downloadURL https://raw.githubusercontent.com/n00b42/userscript/main/scripts/9gag-video-filter.user.js
+//
+// @include     https://9gag.com/*
+// @grant       GM_log
+//
+// @require     https://raw.githubusercontent.com/n00b42/userscript-utilities/main/detectElements.js
+//
+// ==/UserScript==
+
+// ***********************************************************************************
+
+detectElements("article .post-container video", (e) => {
+	e.closest('article').remove();
+})
+
+console.log('9gag vide filter loaded.');
